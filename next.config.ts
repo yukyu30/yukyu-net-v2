5874a7bc39d9f0cb6aa879a412e110858f382f76
yu5 <fukanoyugo+dev@gmail.com>
2025-08-06 02:32:10 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@swc/core-linux-x64-gnu',
+      'node_modules/@swc/core-linux-x64-musl',
+      'node_modules/@esbuild/linux-x64',
+      'public_articles/**/*',
+    ],
+    '/posts/[slug]': ['public_articles/**/*'],
+  },
   async rewrites() {
     return [
       {
